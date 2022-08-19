ea800fbb2bfb195674142df7a0136350e0810472
AqilNoor <aqilnoor.wyrtual.io@gmail.com>
2022-08-19 13:18:26 +0500

    Player script

--- Assets/Scripts/Player Movements/Player.cs
+++ Assets/Scripts/Player Movements/Player.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private CharacterController character_Controller;
+    private Vector3 move_Direction;
+    public float speed = 5f;
+    private float gravity = 20f;
+    public float jump_Force = 10f;
+    private float vertical_Velocity;
+
+    private void Awake()
     {
-        
+        // storing charactercontroller component
+        character_Controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        MoveThePlayer();
+    }
+
+    void MoveThePlayer()
+    {    // Axis.HORIZONTAL and Axis.VERTICAL getting from TagHolder Script
+        move_Direction = new Vector3(Input.GetAxis(Axis.HORIZONTAL), 0f, Input.GetAxis(Axis.VERTICAL));
+
+        //
+        move_Direction = transform.TransformDirection(move_Direction);
+
+
+        move_Direction *= speed * Time.deltaTime;
+        ApplyGravity();
+        // CharacterController.Move() manage all the movements 
+        character_Controller.Move(move_Direction);
+
+
+    }
+
+    void ApplyGravity()
+    {
+        // keep the player on ground
+        vertical_Velocity -= gravity * Time.deltaTime;
+        PlayerJump();
+
         
+        move_Direction.y = vertical_Velocity * Time.deltaTime;
+
+    }
+
+    void PlayerJump()
+    {
+        // player must be on ground and SPACE key is pressed 
+        if (character_Controller.isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            vertical_Velocity = jump_Force;
+        }
     }
 }
