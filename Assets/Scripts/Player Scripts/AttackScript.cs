dfb1174750be902f9103e6cc0e4b76ac42e532b7
AqilNoor <aqilnoor.wyrtual.io@gmail.com>
2022-08-30 19:05:22 +0500

    EnemyAttack Script

---
+++ Assets/Scripts/Player Scripts/AttackScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackScript : MonoBehaviour
+{
+    public float damage = 2f;
+    public float radius = 1f;
+    public LayerMask layerMask;
+
+    void Update()
+    {
+        // this code will creat a sphere of given radius at given position
+        // // and detect the collision layermask which is equal to Enemy
+        // any gameobject which has Enemy Layer
+        Collider[] hits = Physics.OverlapSphere(transform.position, radius, layerMask);
+        // it detects at least on collision
+        if(hits.Length > 0)
+        {
+            print("We Touched;" + hits[0].gameObject.tag);
+            // deactivate that gameobject
+            gameObject.SetActive(false);
+        }
+    }
+}
