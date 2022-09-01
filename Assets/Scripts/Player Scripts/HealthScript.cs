f801ca0d2bb29cda70ba006ffba913f475bc1ab9
AqilNoor <aqilnoor.wyrtual.io@gmail.com>
2022-09-01 17:07:35 +0500

    comments added in HealthScript

--- Assets/Scripts/Player Scripts/HealthScript.cs
+++ Assets/Scripts/Player Scripts/HealthScript.cs
@@ -30,33 +30,41 @@ public class HealthScript : MonoBehaviour
         // if we died dont execute rest of the Code
         if (isDead)
             return;
+
         health -= damage;
+
         if (isPlayer)
         {
             // show the state(display the health UI value
 
         }
+        
         if (isCannible || isBoar)
         {
+            // if enemy is patrolling far away from player and player shoot 
             if (enemyController.EnemyState == EnemyState.PATROL)
             {
+                // enemy will notic the shoot and chase the player 
                 enemyController.chaseDistance = 50f;
             }
         }
+
         if (health <= 0f)
         {
             PlayerDied();
             isDead = true;
         }
-        
+
     }
 
     void PlayerDied()
     {
         if (isCannible)
         {
+            // cannible does not have dead animation
             GetComponent<Animator>().enabled = false;
             GetComponent<BoxCollider>().isTrigger = false;
+            // AddTurque() applys angular force to gameObject
             GetComponent<Rigidbody>().AddTorque(-transform.forward * 50f);
             enemyController.enabled = false;
             navAgent.enabled = false;
@@ -64,35 +72,44 @@ public class HealthScript : MonoBehaviour
             // start coroutine
             // EnemyManager spawn more enemies
         }
+
         if (isBoar)
         {
+            // Boar has a dead animation
             navAgent.velocity = Vector3.zero;
             navAgent.isStopped = true;
             enemyController.enabled = false;
             enemyAnimator.Dead();
             // Spawn more enemies
         }
+
         if (isPlayer)
         {
+            // when player is died all enemies will be called inside enemies[]
             GameObject[] enemies = GameObject.FindGameObjectsWithTag(Tags.ENEMY_TAG);
+
             for (int i = 0; i < enemies.Length; i++)
             {
+                //every kind of action of the enemies will be disabled
                 enemies[i].GetComponent<EnemyController>().enabled = false;
                 // call Enemy Manager to stop spawning enemies
             }
-                GetComponent<PlayerMovement>().enabled = false;
-                GetComponent<PlayerAttack>().enabled = false;
-                GetComponent<WeaponManager>().GetCurrentSelectedWeapon().gameObject.SetActive(false);
+
+            GetComponent<PlayerMovement>().enabled = false;
+            GetComponent<PlayerAttack>().enabled = false;
+            GetComponent<WeaponManager>().GetCurrentSelectedWeapon().gameObject.SetActive(false);
         }
+
+        // when player is died, Restart the game
         if (tag == Tags.PLAYER_TAG)
         {
             Invoke("RestartGame", 3f);
         }
         else
         {
+            // when the any enemy will die, it will deactivate 
             Invoke("TurnOffGame", 3f);
         }
-
     }
     void RestartGame()
     {
